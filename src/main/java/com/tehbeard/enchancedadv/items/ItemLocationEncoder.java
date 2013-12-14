e02e282b0ee2ceed299ab45ca7d8cb910215636a
James <beardyjames@live.co.uk>
2013-12-14 17:27:29 +0000

    Doesn't work as intended, need to packetize? Stopping for the night.

--- src/main/java/com/tehbeard/enchancedadv/items/ItemLocationEncoder.java
+++ src/main/java/com/tehbeard/enchancedadv/items/ItemLocationEncoder.java
@@ -1,9 +1,16 @@
 package com.tehbeard.enchancedadv.items;
 
+import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityCommandBlock;
+import net.minecraft.util.ChatMessageComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
 
@@ -28,10 +35,49 @@ public class ItemLocationEncoder extends Item{
 	}
 	
 	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack,
+			EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
+		
+		initTag(stack);
+		par3List.add("x: " + getLocTag(stack).getInteger("x"));
+		par3List.add("y: " + getLocTag(stack).getInteger("y"));
+		par3List.add("z: " + getLocTag(stack).getInteger("z"));
+	}
+	
+	@Override
 	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player,
 			World world, int x, int y, int z, int side, float hitX, float hitY,
 			float hitZ) {
 		initTag(stack);
+		
+		TileEntity te = world.getBlockTileEntity(x, y, z);
+		//Process teleport commands
+		if(te instanceof TileEntityCommandBlock){
+			
+			
+			TileEntityCommandBlock cmdBlock = ((TileEntityCommandBlock) te);
+			String command = cmdBlock.getCommand();
+			String[] parts = command.split(" ");
+			if(parts[0].equalsIgnoreCase("tp") && parts.length==5){
+				parts[2] = "" + getLocTag(stack).getInteger("x");
+				parts[3] = "" + getLocTag(stack).getInteger("y");
+				parts[4] = "" + getLocTag(stack).getInteger("z");
+			}
+			player.sendChatToPlayer(new ChatMessageComponent().addText("Set teleport command to coordinates" +
+					getLocTag(stack).getInteger("x") + ", " +
+					getLocTag(stack).getInteger("y") + ", " +
+					getLocTag(stack).getInteger("z")
+					
+			));
+			StringBuilder builder = new StringBuilder();
+			for(String s : parts) {
+				if(builder.length() > 0){builder.append(" ");}
+			    builder.append(s);
+			}
+			cmdBlock.setCommand(builder.toString());
+			return true;
+		}
 		if(player.isSneaking()){
 			ForgeDirection direction = ForgeDirection.getOrientation(side);
 			getLocTag(stack).setInteger("x",x + direction.offsetX);
